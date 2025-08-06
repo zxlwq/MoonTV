e8bbf5041207a0514e213989a5db17356e723bf1
shinya <kobayashi_shinya@outlook.com>
2025-08-06 18:46:50 +0800

    feat: add apple-touch-icon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -95,6 +95,7 @@ export default async function RootLayout({
           name='viewport'
           content='width=device-width, initial-scale=1.0, viewport-fit=cover'
         />
+        <link rel='apple-touch-icon' href='/icons/icon-192x192.png' />
         {/* 将配置序列化后直接写入脚本，浏览器端可通过 window.RUNTIME_CONFIG 获取 */}
         {/* eslint-disable-next-line @next/next/no-sync-scripts */}
         <script
